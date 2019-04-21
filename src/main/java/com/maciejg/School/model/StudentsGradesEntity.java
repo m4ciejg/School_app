8bbfa35830c80a9712f0ed1dab4ef0965ed9cac9
unknown <maciekgrabowski1996@gmail.com>
2019-04-21 10:44:37 +0200

    Update DataBase

---
+++ src/main/java/com/maciejg/School/model/StudentsGradesEntity.java
@@ -0,0 +1,71 @@
+package com.maciejg.School.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="studentsGrades")
+public class StudentsGradesEntity {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@NotNull
+	private String name;
+	
+	@NotNull
+	private String surname;
+	
+	@NotNull
+	private int grade;
+	
+	private String comment;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+	
+}
--- src/main/java/com/maciejg/School/model/StudentsTeachersEntity.java
+++ src/main/java/com/maciejg/School/model/StudentsTeachersEntity.java
@@ -4,9 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Table(name="UsersInfo")
 public class StudentsTeachersEntity {
 
 	@Id
@@ -20,11 +22,72 @@ public class StudentsTeachersEntity {
 	private String surname;
 	
 	@NotBlank
-	private String role;
+	private String occupation;
 	
 	@NotBlank
 	private int pesel;
 	
+	@NotBlank
+	private int phoneNumber;
+	
+	@NotBlank
+	private String address;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getOccupation() {
+		return occupation;
+	}
+
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+
+	public int getPesel() {
+		return pesel;
+	}
+
+	public void setPesel(int pesel) {
+		this.pesel = pesel;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	
 	
 	
